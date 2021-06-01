9bd213ebad54837c79965587cddd09bc9c93b711
SlenPlayz <>
2021-06-01 13:09:45 +0000

    New projects folder these projects help me learn html javascript and css

---
+++ projects/change-website-bg/script.js
@@ -0,0 +1,17 @@
+console.log("JS is linked properly");
+function white(){
+  console.log("White");
+  document.body.style.backgroundColor= "White";
+}
+function Teal(){
+  console.log("Teal");
+  document.body.style.backgroundColor= "Teal";
+}
+function Blue(){
+  console.log("Blue");
+  document.body.style.backgroundColor= "#05f";
+}
+function Black(){
+  console.log("Black");
+  document.body.style.backgroundColor= "Black";
+}
